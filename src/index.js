2650bca9b3f5bcf41574cb6169de1788fc93bc3a
Mudar <mudarabofakher@gmail.com>
2023-10-09 12:47:15 +0300

    undo vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import { Analytics } from '@vercel/analytics/react';
-
-const analytics = Analytics('your-vercel-project-id');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Analytics.Provider value={analytics}>
-      <App />
-    </Analytics.Provider>
+    <App />
   </React.StrictMode>
 );
